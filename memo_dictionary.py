fe8970f8e6d89414e14398d84ca7871248026301
Alan Anderson <alan.anderson@udacity.com>
2019-01-11 09:11:28 -0800

    Secure Code

--- memo_dictionary.py
+++ memo_dictionary.py
@@ -1,5 +1,7 @@
 from mw_api.mw import *
 from db import *
+from time import sleep
+from os import getenv
 
 words = get_real_words()
 non_words = get_non_words()
@@ -9,13 +11,23 @@ def is_a_word(word):
   global non_words
   if word in words: return True
   if word in non_words: return False
-  try:
-    dictionary = CollegiateDictionary('9ec3c5f4-167f-43b0-8351-dd8c8661bea3')
-    dictionary.lookup(word)
-    words.add(word)
-    add_real_word(word)
-    return True
-  except WordNotFoundException as err:
-    non_words.add(word)
-    add_non_word(word)
-    return False
+  api_key = getenv('MERRIAM_WEBSTER_COLLEGIATE_KEY')
+  retry = 0
+  while retry < 3:
+    try:
+      dictionary = CollegiateDictionary(api_key)
+      dictionary.lookup(word)
+      words.add(word)
+      add_real_word(word)
+      return True
+    except WordNotFoundException as err:
+      non_words.add(word)
+      add_non_word(word)
+      return False
+    except TypeError as err:
+      if retry >= 3: raise err
+      print(err)
+      print("sleeping for 60 seconds")
+      sleep(60)
+      retry += 1
+
